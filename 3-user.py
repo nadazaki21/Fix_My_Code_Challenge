56dfe3ea48ee71fcc6946144661f1551db8318b7
nadazkai21 <nadazaki019@gmail.com>
2024-03-24 15:50:22 +0200

    3

--- 3-user.py
+++ 3-user.py
@@ -84,20 +84,20 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right \
+    if user_1.is_valid_password(u_pwd):
+        print("1is_valid_password should return True if it's the right \
 password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right \
+        print("2is_valid_password should return False if it's not the right \
 password")
 
     if user_1.is_valid_password(None):
-        print("is_valid_password should return False if compare with None")
+        print("3is_valid_password should return False if compare with None")
 
     if user_1.is_valid_password(89):
-        print("is_valid_password should return False if compare with integer")
+        print("4is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no password set \
+        print("5is_valid_password should return False if no password set \
 before")
