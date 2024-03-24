1cf321ff31dfabafaeeca93029a9dfb4f61bd4e3
nadazkai21 <nadazaki019@gmail.com>
2024-03-24 16:20:36 +0200

    3

--- 3-user.py
+++ 3-user.py
@@ -13,7 +13,7 @@ class User():
     - password: private string hash in MD5
     """
 
-    __password = None
+    
 
     def __init__(self):
         """
@@ -21,6 +21,7 @@ class User():
         - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
+        self.__password = None
 
     @property
     def password(self):
@@ -40,7 +41,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +55,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return False
+        return (hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password)
 
 
 if __name__ == '__main__':
@@ -85,19 +86,19 @@ if __name__ == '__main__':
         print("User.password should be None if setter to an integer")
 
     if user_1.is_valid_password(u_pwd):
-        print("1is_valid_password should return True if it's the right \
+        print("is_valid_password should return True if it's the right \
 password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("2is_valid_password should return False if it's not the right \
+        print("is_valid_password should return False if it's not the right \
 password")
 
     if user_1.is_valid_password(None):
-        print("3is_valid_password should return False if compare with None")
+        print("is_valid_password should return False if compare with None")
 
     if user_1.is_valid_password(89):
-        print("4is_valid_password should return False if compare with integer")
+        print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("5is_valid_password should return False if no password set \
+        print("is_valid_password should return False if no password set \
 before")
